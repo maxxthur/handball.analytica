28af95ba5f88bc1c43369f8faf94fa8713e96792
Max Thur <maxthur95@gmail.com>
2019-01-02 17:16:41 +0100

    contuuation

--- R/Spielverlaufsanalyse.R
+++ R/Spielverlaufsanalyse.R
@@ -10,14 +10,14 @@
 #' @examples
 Tore_Spielminute_Spieler <- function(Name, Team, Data) {
   Tore_Spieler <- Data[[3]] %>%
-    filter(Spieler==Name) %>%
-    mutate(Spielabschnitt=as.numeric(substr(Spielminute, 1,1))+1) %>%
-    group_by(Datum) %>% count(Spielabschnitt)
+    dplyr::filter(Spieler==Name) %>%
+    dplyr::mutate(Spielabschnitt=as.numeric(substr(Spielminute, 1,1))+1) %>%
+    dplyr::group_by(Datum) %>% dplyr::count(Spielabschnitt)
 
   Tore_Mannschaft <- Data[[1]] %>%
-    filter(Mannschaft==Team) %>%
-    group_by(Datum) %>%
-    summarise(Tore=sum(Tore))
+    dplyr::filter(Mannschaft==Team) %>%
+    dplyr::group_by(Datum) %>%
+    dplyr::summarise(Tore=sum(Tore))
 
   Mean_Tore_Mannschaft <- mean(Tore_Mannschaft$Tore)
   VC_Tore_Mannschaft <- sd(Tore_Mannschaft$Tore)/mean(Tore_Mannschaft$Tore)
@@ -26,24 +26,24 @@ Tore_Spielminute_Spieler <- function(Name, Team, Data) {
 
   for(i in (1:length(unique(Tore_Spieler$Datum)))) {
     Gegn <- as.character(unique(Data[[1]] %>%
-                                  filter(Datum==unique(Tore_Spieler$Datum)[i] & Mannschaft==Team) %>%
+                                  dplyr::filter(Datum==unique(Tore_Spieler$Datum)[i] & Mannschaft==Team) %>%
                                   .$Gegner))
 
     Mean_Gegentore_Gegner <- mean(Data[[1]] %>%
-                                    filter(Gegner==Gegn) %>%
-                                    group_by(Datum)  %>%
-                                    summarise(Gegentore=sum(Tore)) %>%
+                                    dplyr::filter(Gegner==Gegn) %>%
+                                    dplyr::group_by(Datum)  %>%
+                                    dplyr::summarise(Gegentore=sum(Tore)) %>%
                                     .$Gegentore)
 
     VC_Gegentore_Gegner <- sd(Data[[1]] %>%
-                                filter(Gegner==Gegn) %>%
-                                group_by(Datum)  %>%
-                                summarise(Gegentore=sum(Tore)) %>%
+                                dplyr::filter(Gegner==Gegn) %>%
+                                dplyr::group_by(Datum)  %>%
+                                dplyr::summarise(Gegentore=sum(Tore)) %>%
                                 .$Gegentore)/Mean_Gegentore_Gegner
 
 
-    Tore_erzielt_Spieler <- Tore_Spieler %>% filter(Datum==unique(Tore_Spieler$Datum)[i])
-    Tore_erzielt_Mannschaft <- Tore_Mannschaft %>% filter(Datum==unique(Tore_Spieler$Datum)[i])
+    Tore_erzielt_Spieler <- Tore_Spieler %>% dplyr::filter(Datum==unique(Tore_Spieler$Datum)[i])
+    Tore_erzielt_Mannschaft <- Tore_Mannschaft %>% dplyr::filter(Datum==unique(Tore_Spieler$Datum)[i])
     Tore_erzielt_Spieler$n <- (Tore_erzielt_Spieler$n/Tore_erzielt_Mannschaft$Tore)*((Mean_Tore_Mannschaft+Mean_Gegentore_Gegner)*(VC_Gegentore_Gegner/VC_Tore_Mannschaft)/2)
 
     for(j in (1:nrow(Tore_erzielt_Spieler))) {
@@ -68,7 +68,8 @@ Tore_Spielminute_Spieler <- function(Name, Team, Data) {
     ggplot2::theme(panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(),
                    panel.background = ggplot2::element_blank(), axis.line = ggplot2::element_line(colour = "black")) +
     ggplot2::xlab("Spielabschnitt") +
-    ggplot2::ylab("Torgefährlichkeit")
+    ggplot2::ylab("Torgefährlichkeit") +
+    ggplot2::ggtitle(label="Torgefährlichkeit laufende Saison")
 }
 
 
