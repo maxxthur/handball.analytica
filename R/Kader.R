fccf2732c2f1a3f03ebbf419ec1c58121d5c82f9
Max Thur <maxthur95@gmail.com>
2019-01-03 15:09:54 +0100

    Implemented new analysis of the opponents team stats

--- R/Kader.R
+++ R/Kader.R
@@ -2,25 +2,47 @@
 #'
 #' @param Team
 #' @param Data
-#' @param Matches
+#' @param ordering
 #'
 #' @return
 #' @export
 #'
 #' @examples
-Kader <- function(Team, Data, Matches) {
-  TeamData <- TeamData <- Data[[1]] %>% dplyr::filter(Mannschaft == Team)
-  Kader_Voll <- TeamData %>% dplyr::group_by(Name, Mannschaft) %>% dplyr::summarise(Tore = sum(Tore), Zeitstrafen = sum(`2min`), V = sum(V)) %>% dplyr::select("Name", "Tore", "Zeitstrafen", "V")
-  Spieltage <- unique(TeamData$Datum)
-  Kader_Rest <- list()
-  for (i in ((length(Spieltage) - Matches):length(Spieltage))) {
-    TeamData_Spieltag <- TeamData %>% dplyr::filter(Datum == Spieltage[i])
-    Kader_Spieltag <- TeamData_Spieltag %>% dplyr::select("#", "Name", "Tore", "2min", "V")
-    Kader_Rest[[i - (length(Spieltage) - Matches - 1)]] <- Kader_Spieltag
-  }
-  Meiste_Tore <- Kader_Voll %>% dplyr::arrange(dplyr::desc(Tore)) %>% .[1:3,1:2] %>% dplyr::mutate(Abh.=round(Tore/sum(Kader_Voll$Tore), 3))
-  Meiste_Zeitstrafen <-  Kader_Voll %>% dplyr::arrange(dplyr::desc(Zeitstrafen)) %>% .[1:3,c(1,3)]
-  Meiste_Gelbe <- Kader_Voll %>% dplyr::arrange(dplyr::desc(V)) %>% .[1:3,c(1,4)] %>% dplyr::mutate(Gelbe=V) %>% dplyr::select("Name", "Gelbe")
+Kader <- function(Team, Data, order) {
+  TeamData <- TeamData <- Data[[1]] %>%
+    dplyr::filter(Mannschaft == Team & !(`#` %in% c("OA", "OB", "OC", "OD", "OE", "OF")))
 
-  list(Ganzer_Kader = Kader_Voll, Spieltagskader = Kader_Rest, Meiste_Tore=Meiste_Tore, Meiste_Zeitstrafen=Meiste_Zeitstrafen, Meiste_Gelbe=Meiste_Gelbe)
-} # hier überlegen, ob es Sinn macht nicht fehlende Spieler ausgeben zu lassen und nicht den Spieltagskader, zusätzlich könnte man noch die erzielten Tore ausgeben um formstarke Spieler zu identifizieren
+  Kader_Voll <- TeamData %>%
+    dplyr::group_by(Name, Mannschaft) %>%
+    dplyr::summarise(Tore = sum(Tore) , Zeitstrafen = sum(`2min`), V = sum(V)) %>%
+    dplyr::mutate("% Tore"=Tore/sum(.$Tore)) %>%
+    dplyr::select("Name", "Tore","% Tore", "Zeitstrafen", "V")
+
+  Tore_Mannschaft <- Data[[1]] %>% dplyr::filter(Mannschaft==Team) %>% dplyr::group_by(Datum) %>% dplyr::summarise(Tore=sum(Tore))
+
+  Abhängigkeit <- data.table::data.table()
+
+  for(i in (1:length(Kader_Voll$Name))) {
+    Tore_Spieler <- Data[[1]] %>% dplyr::filter(Name==Kader_Voll$Name[i])
+    Tore_M_S <- cbind(Tore_Mannschaft %>% dplyr::filter(Datum %in% Tore_Spieler$Datum), Tore_S=Tore_Spieler$Tore)
+    Korrelation <- cor(Tore_M_S$Tore, Tore_M_S$Tore_S)
+    Abh <- round(Kader_Voll$`% Tore`[i]*(1-Korrelation), 3)
+    Abhängigkeit <- rbind(Abhängigkeit,cbind(Name=Kader_Voll$Name[i], Abhängigkeit=Abh))
+  }
+
+  Output <- dplyr::left_join(Kader_Voll, Abhängigkeit)
+  ordering = if (order=="Tore") {
+    Output$Tore
+  } else if (order=="Abhängigkeit") {
+    Output$Abhängigkeit
+  } else if (order=="Zeitstrafen") {
+    Output$Zeitstrafen
+  } else if (order=="Gelbe Karten") {
+    Output$V
+  } else {Output$Tore}
+
+  Output %>% dplyr::arrange(desc(ordering))
+}
+
+
+
