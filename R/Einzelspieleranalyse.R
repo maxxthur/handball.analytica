00cc267deff35b88d4bee6b9ab106e9def7b5095
Max Thur <maxthur95@gmail.com>
2019-01-04 16:28:11 +0100

    fixed bugs

--- R/Einzelspieleranalyse.R
+++ R/Einzelspieleranalyse.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param Player
+#' @param Team
+#' @param Data
+#'
+#' @return
+#' @export
+#'
+#' @examples
 Einzelspieleranalyse <- function(Player, Team, Data){
   Mean_Tore <- mean(
     Data[[1]] %>%
@@ -26,7 +36,7 @@ Einzelspieleranalyse <- function(Player, Team, Data){
   Fundamentals <- data.table::data.table("durchsch. Tore"=Mean_Tore, "Zeitstrafen/Saison"=Zeitstrafen_Gesamt, "Gelbe Karten/Saison"=Gelbe_Gesamt)
 
   Tore_Spiele <- Data[[1]] %>%
-    dplyr::filter(Name=="RUHNKE, JOERG") %>%
+    dplyr::filter(Name==Player) %>%
     dplyr::group_by(Datum, Gegner) %>%
     dplyr::summarise(Tore=Tore) %>%
     ggplot2::ggplot(ggplot2::aes(x=Gegner, y=Tore)) +
@@ -34,10 +44,10 @@ Einzelspieleranalyse <- function(Player, Team, Data){
     ggplot2::theme(panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_blank(), axis.line = ggplot2::element_line(colour = "black"))+
     ggplot2::xlab("Gegner") +
     ggplot2::ylab("Tore") +
-    ggplot2::ggtitle(label=paste("Tore ", "RUHNKE, JOERG"))
+    ggplot2::ggtitle(label=paste("Tore ", Player))
 
   Zeitstrafen_Spiele <- Data[[1]] %>%
-    dplyr::filter(Name=="RUHNKE, JOERG") %>%
+    dplyr::filter(Name==Player) %>%
     dplyr::group_by(Datum, Gegner) %>%
     dplyr::summarise(Zeitstrafen=`2min`) %>%
     ggplot2::ggplot(ggplot2::aes(x=Gegner, y=Zeitstrafen)) +
@@ -45,10 +55,10 @@ Einzelspieleranalyse <- function(Player, Team, Data){
     ggplot2::theme(panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_blank(), axis.line = ggplot2::element_line(colour = "black"))+
     ggplot2::xlab("Gegner") +
     ggplot2::ylab("Zeitstrafen") +
-    ggplot2::ggtitle(label=paste("Zeitstrafen ", "RUHNKE, JOERG"))
+    ggplot2::ggtitle(label=paste("Zeitstrafen ", Player))
 
   Gelbe_Spiele <- Data[[1]] %>%
-    dplyr::filter(Name=="RUHNKE, JOERG") %>%
+    dplyr::filter(Name==Player) %>%
     dplyr::group_by(Datum, Gegner) %>%
     dplyr::summarise(Gelbe=V) %>%
     ggplot2::ggplot(ggplot2::aes(x=Gegner, y=Gelbe)) +
@@ -56,11 +66,11 @@ Einzelspieleranalyse <- function(Player, Team, Data){
     ggplot2::theme(panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_blank(), axis.line = ggplot2::element_line(colour = "black"))+
     ggplot2::xlab("Gegner") +
     ggplot2::ylab("Gelbe Karten") +
-    ggplot2::ggtitle(label=paste("Gelbe Karten ", "RUHNKE, JOERG"))
+    ggplot2::ggtitle(label=paste("Gelbe Karten ", Player))
 
-  Torgefahr <- handball.analytica::Tore_Spielminute_Spieler("RUHNKE, JOERG", "DJK Grün-Weiß Nottuln", Data)
-  Zeitstrafen <- handball.analytica::Zeitstrafen_Spielverlauf_Spieler("RUHNKE, JOERG", Data)
-  Gelbe <- handball.analytica::GelbeKarten_Spielverlauf_Spieler("RUHNKE, JOERG", Data)
+  Torgefahr <- handball.analytica::Tore_Spielminute_Spieler(Player, Team, Data)
+  Zeitstrafen <- handball.analytica::Zeitstrafen_Spielverlauf_Spieler(Player, Data)
+  Gelbe <- handball.analytica::GelbeKarten_Spielverlauf_Spieler(Player, Data)
 
   plot1 <- ggpubr::ggarrange(Torgefahr, Zeitstrafen, Gelbe, ncol=3, nrow=1)
   plot <- ggpubr::ggarrange(Tore_Spiele, Zeitstrafen_Spiele, Gelbe_Spiele, plot1, ncol=1, nrow=4)
